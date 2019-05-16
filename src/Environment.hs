acb3c200d4b7ee473732541bf516afb44c3a09e3
styczynski <piotr@styczynski.in>
2019-05-16 16:30:28 +0100

    Add environment helpers

--- src/Environment.hs
+++ src/Environment.hs
@@ -8,3 +8,18 @@ data RuntimeValue = RUnit | RInt Integer | RString String deriving (Show, Eq)
 data Environment = Environment { variables :: (Map.Map Ident RuntimeValue) } deriving (Show)
 
 emptyEnv = Environment { variables=Map.empty }
+
+setVariable :: Environment -> Ident -> RuntimeValue -> Environment
+setVariable env name val =
+  let Environment { variables = oldVariables } = env in
+  env { variables = (Map.insert name val oldVariables) }
+
+delVariable :: Environment -> Ident -> Environment
+delVariable env name =
+  let Environment { variables = oldVariables } = env in
+  env { variables = (Map.delete name oldVariables) }
+
+getVariable :: Environment -> Ident -> RuntimeValue
+getVariable env name =
+  let Environment { variables = oldVariables } = env in
+  Map.findWithDefault RUnit name oldVariables
