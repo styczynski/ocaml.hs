12f500bd39e470354a3c961263fff6f29d660680
styczynski <piotr@styczynski.in>
2019-06-17 20:18:58 +0100

    Improve fuzzy search example with custom sorting

--- examples/good/FuzzySearch.ml
+++ examples/good/FuzzySearch.ml
@@ -12,9 +12,29 @@ let rec levenshtein s t =
    in
    dist (length s) (length t)
 
-let rec test s t =
-  printf " %s -> %s = %d\n" s t (levenshtein s t)
+let rec levenshtein_generate_score i l = match l with
+        | [] -> []
+        | (h::t) ->
+            let score = levenshtein i h in
+            let subscore = levenshtein_generate_score i t in
+            cons (h,score) subscore
+
+let rec insert cmp e l = match l with
+    | [] -> [e]
+    | (h :: t) -> if cmp e h <= 0 then cons e (cons h t) else cons h (insert cmp e t)
+    | xxx -> print xxx ~ failwith "EEE1"
+
+let rec sort cmp l = match l with
+    | [] -> []
+    | (h :: t) -> insert cmp h (sort cmp t)
+    | xxx -> print xxx ~ failwith "EEE2"
+
+let rec levenshtein_score i l =
+    let scores = levenshtein_generate_score i l in
+    let scores_sorted = sort (fun (_, v1) (_, v2) -> if v1 < v2 then 0-1 else if v1 > v2 then 1 else 0) scores in
+    let get_first (s,_) = s in
+    map get_first (take 5 scores_sorted)
 
 ;;
 
-test ["k";"i";"t";"t";"e";"n"] ["s";"i";"t";"t";"i";"n";"g"]
+levenshtein_score ["k";"i";"t";"t";"e";"n"] [["s";"i";"t";"t";"i";"n";"g"];["x";"y";"z"]]
