df3c82337e2da4d407b3f4541327816a14ea5c92
styczynski <piotrsty1@gmail.com>
2019-08-13 15:14:49 +0200

    Add test-preprocessor target

--- Shakefile.hs
+++ Shakefile.hs
@@ -11,14 +11,23 @@ import System.FilePath.Glob
 import Filesystem.Path.CurrentOS (decodeString)
 import Text.Regex.TDFA
 
-grepShCommand :: String -> [String] -> String -> Sh (Maybe [String])
-grepShCommand command args regex = do
+grepShCommand :: String -> [String] -> String -> String -> Sh (Maybe [String])
+grepShCommand command args cwd regex = do
+    cd $ decodeString cwd
     host <- run (decodeString command) $ map pack args
     matches <- return $ filter (\(_, _, _, m) -> length m > 0) $ map (\line -> (line =~ regex) :: (String, String, String, [String])) $ lines $ unpack host
     result <- return $ case matches of
         ((_, _, _, h:t):_) -> Just $ h:t
         _ -> Nothing
     return result
+
+getStackInstallDir :: String -> IO String
+getStackInstallDir path = shelly $ silently $ do
+    stackPathResult <- grepShCommand "stack" ["path", "--allow-different-user"] path "local-install-root: (.*)"
+    stackPath <- return $ case stackPathResult of
+        Nothing -> ""
+        Just (h:_) -> h
+    return stackPath
 
 executeCommand :: String -> [String] -> String -> IO String
 executeCommand command args cwd = shelly $ do
@@ -31,13 +40,10 @@ executeCommandStack args = executeCommand "stack" (["--allow-different-user"] ++
 
 mainFn :: Sh ()
 mainFn = do
-    stackPathResult <- grepShCommand "stack" ["path", "--allow-different-user"] "local-install-root: (.*)"
-    stackPath <- return $ case stackPathResult of
-        Nothing -> ""
-        Just (h:_) -> h
+
     liftIO $ shakeArgs shakeOptions{shakeFiles="_build"} $ do
 
-        want ["_build/interpreter" <.> exe]
+        want ["_build/test-preprocessor" <.> exe]
 
         ["parser/*.hs"] &%> \[ouths] -> do
             _ <- liftIO $ executeCommandStack ["exec", "bnfc", "--", "-m", "-o", "parser", "./grammar/syntax.cf"] "."
@@ -51,6 +57,7 @@ mainFn = do
             liftIO $ putStrLn "Done!"
 
         "_build/interpreter" <.> exe %> \out -> do
+            stackPath <- liftIO $ getStackInstallDir "."
             _ <- liftIO $ executeCommandStack ["upgrade", "--binary-version", "2.1.1"] "."
             _ <- liftIO $ executeCommandStack ["install", "--only-dependencies"] "."
             _ <- liftIO $ executeCommandStack ["install", "happy"] "."
@@ -60,7 +67,13 @@ mainFn = do
             grammarFiles <- liftIO $ glob "grammar/**/*.cf"
             need $ srcFiles ++ appFiles ++ grammarFiles
             _ <- liftIO $ executeCommandStack ["build"] "."
-            _ <- liftIO $ shelly $ cp (decodeString $ stackPath </> "bin" </> "interpreter") (decodeString "_build/interpreter")
+            _ <- liftIO $ shelly $ cp (decodeString $ stackPath </> "bin" </> "interpreter") (decodeString out)
+            return ()
+
+        "_build/test-preprocessor" <.> exe %> \out -> do
+            preprocessorStackPath <- liftIO $ getStackInstallDir "test-preprocessor"
+            _ <- liftIO $ executeCommandStack ["build"] "test-preprocessor"
+            _ <- liftIO $ shelly $ cp (decodeString $ preprocessorStackPath </> "bin" </> "test-preprocessor") (decodeString out)
             return ()
 
 main :: IO ()
