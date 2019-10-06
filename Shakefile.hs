ff70f24d204849aab0a149287e9f38268abb4b96
Piotr Aleksander Styczyński <piotrsty1@gmail.com>
2019-10-06 13:51:29 +0200

    Try to fix Travis (repair targets parralerism crashes)

--- Shakefile.hs
+++ Shakefile.hs
@@ -7,15 +7,16 @@ main :: IO ()
 main = do
     stackDir <- getStackInstallDir "."
     executeTasks $ do
-        want [("_build/cli" <.> exe)] --["test.lock", "_build/web"]
+        want ["_build/web"]
 
---         "test.lock" %> \out -> do
---             alwaysRerun
---             executeCommandStack ["upgrade", "--binary-version", "2.1.3"] "."
---             success <- executeStackBuild ["test", "--no-terminal", "--coverage"] "."
---             _ <- liftIO $ if success then return () else ioError $ userError "Fail."
---             executeCommand "touch" [out] "."
---             return ()
+        "test.lock" %> \out -> do
+            need [("_build/cli" <.> exe)]
+            alwaysRerun
+            executeCommandStack ["upgrade", "--binary-version", "2.1.3"] "."
+            success <- executeStackBuild ["test", "--no-terminal", "--coverage"] "."
+            _ <- liftIO $ if success then return () else ioError $ userError "Fail."
+            executeCommand "touch" [out] "."
+            return ()
 
         ["_build/cli"] &%> \[ouths] -> do
             executeCommandStack ["upgrade", "--binary-version", "2.1.1"] "."
@@ -24,8 +25,9 @@ main = do
             executeCommand "cp" ([(stackDir </> "bin" </> "interpreter"), ("_build/cli" <.> exe)]) "."
             message "Done!"
 
---         ["_build/web"] &%> \[ouths] -> do
---             executeCommandStack ["upgrade", "--binary-version", "2.1.3"] "."
---             executeCommandStack ["exec", "--cwd", "./packages/web", "--", "shake"] "."
---             --executeCommandStack ["exec", "--cwd", "./packages/web-frontend", "--", "shake"] "."
---             message "Done web!"
+        ["_build/web"] &%> \[ouths] -> do
+            need ["test.lock"]
+            executeCommandStack ["upgrade", "--binary-version", "2.1.3"] "."
+            executeCommandStack ["exec", "--cwd", "./packages/web", "--", "shake"] "."
+            --executeCommandStack ["exec", "--cwd", "./packages/web-frontend", "--", "shake"] "."
+            message "Done web!"
