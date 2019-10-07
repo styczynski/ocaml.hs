fb72e380eb564af8517a8fdedfd4f1844348398b
Piotr Aleksander Styczyński <piotrsty1@gmail.com>
2019-10-07 18:29:34 +0200

    Try to fix Travis (comment out ocamlhs-docs build script)

--- packages/doc/Setup.hs
+++ packages/doc/Setup.hs
@@ -2,11 +2,11 @@ import Buildtools
 import Distribution.Types.GenericPackageDescription (emptyGenericPackageDescription)
 
 main = do
-    stackDir <- return ".stack-work"
-    executeTasks $ do
-        want ["_build/docs/index.html"]
-        ["_build/docs/*"] &%> \[ouths] -> do
-            executeCommandStack ["exec", "tintin", "run"] "."
-            executeCommand "cp" (["-R", (stackDir </> "tintin" </> "rendered" </> "."), ("_build/docs")]) "."
-            return ()
+--     stackDir <- return ".stack-work"
+--     executeTasks $ do
+--         want ["_build/docs/index.html"]
+--         ["_build/docs/*"] &%> \[ouths] -> do
+--             executeCommandStack ["exec", "tintin", "run"] "."
+--             executeCommand "cp" (["-R", (stackDir </> "tintin" </> "rendered" </> "."), ("_build/docs")]) "."
+--             return ()
     defaultMain
