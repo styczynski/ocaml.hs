29c1c2d52bae144b1c0ca4b0afb5fda661d6dbaf
Piotr Aleksander Styczyński <piotrsty1@gmail.com>
2019-10-07 12:50:07 +0200

    Try to fix Travis (add reconfiguration)

--- Shakefile.hs
+++ Shakefile.hs
@@ -13,7 +13,7 @@ main = do
             need [("_build/cli" <.> exe)]
             alwaysRerun
             executeCommandStack ["upgrade", "--binary-version", "2.1.3"] "."
-            success <- executeStackBuild ["test", "--no-terminal", "--coverage", "--enable-tests"] "."
+            success <- executeStackBuild ["test", "--no-terminal", "--coverage", "--reconfigure"] "."
             _ <- liftIO $ if success then return () else ioError $ userError "Fail."
             executeCommand "touch" [out] "."
             return ()
